1a1b1facd5e094aaa3949fb6a1aa2fe6d677b906
Jpina150 <jpinael0@emai.cpcc.edu>
2023-09-11 23:06:11 -0400

    Added code for demo class

--- Project_Jonathan_PinaElacio.java
+++ Project_Jonathan_PinaElacio.java
@@ -17,8 +17,45 @@ public class Project_Jonathan_PinaElacio
       String providerName = Keyboard.nextLine();
       insurance.setProviderName(providerName);
       
-      System.out.println("Policy Number: " + insurance.getPolicyNumber());
+      System.out.println("Please enter the Policyholder's First Name: ");
+      String firstName = Keyboard.nextLine();
+      insurance.setFirstName(firstName);
+      
+      System.out.println("Please enter the Policyholder's Last Name: ");
+      String lastName = Keyboard.nextLine();
+      insurance.setLastName(lastName);
+      
+      System.out.println("Please enter the Policyholder's Age: ");
+      double age = Keyboard.nextDouble();
+      insurance.setAge(age);
+      
+      Keyboard.nextLine();
+      
+      System.out.println("Please enter the Policyholder's Smoking Status (Smoker/Non-Smoker): ");
+      String smokeStatus = Keyboard.nextLine();
+      insurance.setSmokeStatus(smokeStatus);
+      
+      System.out.println("Please enter the Policyholder's Height (in inches): ");
+      double height = Keyboard.nextDouble();
+      insurance.setHeight(height);
+      
+      System.out.println("Please enter the Policyholder's Weight (in pounds): ");
+      double weight = Keyboard.nextDouble();
+      insurance.setWeight(weight);
+      
+      double BMI = insurance.getBMI();
+      double policyPrice = insurance.getInsurancePrice(BMI);
+      
+      System.out.printf("Policy Number: %.0f%n",  insurance.getPolicyNumber());
       System.out.println("Provider Name: " + insurance.getProviderName());
+      System.out.println("Policyholder's First Name: " + insurance.getFirstName());
+      System.out.println("Policyholder's Last Name: " + insurance.getLastName());
+      System.out.printf("Policyholder's Age: %.0f%n", insurance.getAge());
+      System.out.println("Policyholder's Smoking Status: " + insurance.getSmokeStatus());
+      System.out.printf("Policyholder's Height (in inches): %.1f%n", insurance.getHeight());
+      System.out.printf("Policyholder's BMI: %.2f%n", BMI);
+      System.out.printf("Policy Price: $%.2f%n", policyPrice);
+      
    }
 
 }
